42b073055842af1ddfeab49d8fd02b3916cab5ba
Rajkumar Satish (Moody's) <satishr1@moodys.com>
2024-12-11 18:07:37 +0530

    add express validator in the validation middleware

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -5,24 +5,27 @@
 // class
 // assignment
 
-const validateRequest = (req, res, next)=>{
-    const {name, price, imageUrl} = req.body
-        let errors = [];
-        if(!name || name.trim()==""){
-            errors.push('Invalid name or name cannot be empty')
+import { body, validationResult } from "express-validator";
+
+const validateRequest = async (req, res, next)=>{
+    // 1. setup rules for validationResult
+    const rules = [
+        body("name").notEmpty().withMessage('Name is required!!'),
+        body("price").isFloat({gt:0}).withMessage("Prive should be a positive value!!"),
+        body("imageUrl").isURL().withMessage("Invalid Url")
+        ]
+    // 2. run those rules
+
+    await Promise.all(rules.map(rule=> rule.run(req)))
+
+    // 3. check if there are any error after running the rules
+    var validationErrors = validationResult(req)
+
+
+    if(!validationErrors.isEmpty()){
+            return res.render('new-product',{errorMessage: validationErrors.array()[0].msg} )
         }
-        if(!price || parseFloat(price) < 1){
-            errors.push('Price cannot be negative')
-        }
-        try {
-            const validUrl = new URL(imageUrl)
-        } catch (error) {
-            errors.push('Invalid url')  
-        }
-        if(errors.length > 0){
-            return res.render('new-product',{errorMessage: errors[0]} )
-        }
-        next()
+    next()
 }
 
 export default validateRequest
