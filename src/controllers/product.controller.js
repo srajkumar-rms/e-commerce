0a93bada9d0454e14eeb4ef539fc24955ce126a4
Rajkumar Satish (Moody's) <satishr1@moodys.com>
2024-12-11 17:06:43 +0530

    add data validation

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -13,11 +13,27 @@ export default class ProductController {
     }
 
     getAddForm(req, res){
-        return res.render('new-product')
+        return res.render('new-product', {errorMessage: null})
     }
 
     addNewProduct(req,res){
 
+        const {name, price, imageUrl} = req.body
+        let errors = [];
+        if(!name || name.trim()==""){
+            errors.push('Invalid name or name cannot be empty')
+        }
+        if(!price || parseFloat(price) < 1){
+            errors.push('Price cannot be negative')
+        }
+        try {
+            const validUrl = new URL(imageUrl)
+        } catch (error) {
+            errors.push('Invalid url')  
+        }
+        if(errors.length > 0){
+            return res.render('new-product',{errorMessage: errors[0]} )
+        }
         ProductModel.add(req.body)
         let products = ProductModel.get()
         return res.render("products", {products})
